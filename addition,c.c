ad882355fc24408a2002e52f80f53b46a1235fcd
Mayur Pawar <141900306+Mayur4518@users.noreply.github.com>
2024-02-29 22:07:06 +0530

    Update addition.c

--- addition,c.c
+++ addition,c.c
@@ -1,23 +1,10 @@
 #include<stdio.h>
 int main()
 {
-	int a[2][3],i,j;
-	printf("Enter matrix");
-	for(i=0;i<2;i++)
-	{
-		for(j=0;j<3;j++)
-		{
-			scanf("%d",&a[i][j]);
-		}
-	}
-	printf("\n display matrix:\n");
-	for(i=0;i<2;i++)
-	{
-		for(j=0;j<3;j++)
-		{
-			printf("%d\t",a[i][j]);
-		}
-		printf("\n");
-	}
+	int a,b,c;
+ printf("Enter two numbers");
+ scanf("%d%d",&a,&b);
+ c=a+b;
+ printf("Addition=%d",c);
 }
 
